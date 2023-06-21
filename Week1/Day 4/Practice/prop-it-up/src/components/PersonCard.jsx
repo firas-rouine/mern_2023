e4e1d10b6d3f3308ee69a8d9b25643782cb0c4d4
firas-rouine <rouine.firas@gmail.com>
2023-06-21 18:27:50 +0100

    Prop It Up commit

--- Week1/Day 4/Practice/prop-it-up/src/components/PersonCard.jsx
+++ Week1/Day 4/Practice/prop-it-up/src/components/PersonCard.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-
+import './PersonCard.css';
 export default class PersonCard extends Component {
     render() {
         // console.log(this.props.person)
         const arrayperson = this.props.person
         console.log(arrayperson);
         return (
-            <div>
+            <div className='card'>
                 {arrayperson.map((onePerson, index) => {
-                    return (<div key={index}>
+                    return (<div className='content' key={index}>
                         <h1>{onePerson.first_name},{onePerson.last_name}</h1>
                         <p>Age : {onePerson.age}</p>
                         <p>Hair Color : {onePerson.hair_color}</p>
