8b5b49120d5d14a107f0a834758b67dd0f6eb166
firas-rouine <rouine.firas@gmail.com>
2023-07-07 15:56:16 +0100

    Product Manager (Part I) commit

---
+++ Week3/Day 1/Practice/Product Manager (Part I)/client/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Form from './components/Form';
+import { Routes, Route } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+         <Routes>
+        <Route path='/' element={<Form />} />
+  
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
